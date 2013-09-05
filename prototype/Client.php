544919df113c53bbc5e74d74bcd710a77f9ba72e
Saad Guessous <guessous_saad@hotmail.com>
2013-09-05 18:34:44 +0300

    Cloned movies and interacted successfully with Redis.

--- prototype/Client.php
+++ prototype/Client.php
@@ -16,15 +16,15 @@ include_once('Ebook.php');
 
 Class Client
 {
-	private $movie;
-	private $ebook;
+	private $movie = array();
+	private $ebook = array();
 	private $redis;
 
 	public function __construct()
 	{
 		$this->makeConPrototype();
 		$this->redis = new Predis\Client();
-		$this->initProductId();
+		$this->cloneMovies();
 	}
 
 	private function makeConPrototype()
@@ -33,9 +33,41 @@ Class Client
 		$this->ebook = new Ebook();
 	}
 
-	private function initProductId()
+	private function cloneMovies()
 	{
-		echo $this->redis->llen('movies_id');
+		//lenght of list containing movie id's
+		$var = $this->redis->llen('movies_id');
+
+		$movies_id= array();
+		$movies_data = array();
+		$movieClones = array();
+
+		//We fetch the list of movie id's
+		$movies_id = $this->redis->lrange('movies_id', 0, $var);
+
+		//We fetch data related to each movie
+		for ($i = 0; $i < $var; $i++)
+			$movies_data[] = $this->redis->hgetall("movies:$movies_id[$i]");
+
+		//We clone concrete class and insert data
+		for ($i = 0; $i < $var; $i++)
+		{
+			$movieClones[$i] = clone $this->movie;
+			$movieClones[$i]->setName( $movies_data[$i]['name'] );
+			$movieClones[$i]->setDescription( $movies_data[$i]['description'] );
+		}	
+
+
+		for ($i = 0; $i < $var; $i++)
+		{
+			echo '<strong>'.$movieClones[$i]->getName().'</strong><br />';
+			echo '<p>'.$movieClones[$i]->getDescription().'</p><br /><br />';
+
+		}
+			
+
+
+
 	}
 
 }
