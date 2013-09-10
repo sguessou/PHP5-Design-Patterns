d7b44fa239042c48fe246a3276b0df8bc932d3f7
Saad Guessous <guessous_saad@hotmail.com>
2013-09-10 16:09:06 +0300

    Porting from Java to PHP factory pattern examples found in HF Desing Patterns.

---
+++ HF_factory/simple_factory/SimplePizzaFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+includ_once('Pizza.php');
+
+class SimplePizzaFactory
+{
+	private $pizza;
+
+	public function createPizza($str)
+	{
+		if ($str == 'cheese') {
+			$this->pizza = new CheesePizza();
+		}elseif ($str == 'pepperoni') {
+			$this->pizza = new PepperoniPizza();
+		}elseif ($str == 'clam') {
+			$this->pizza = new ClamPizza();
+		}elseif ($str == 'veggie') {
+			$this->pizza = new VeggiePizza();
+		}
+
+		return $this->pizza;
+	}
+}
+
+?>
